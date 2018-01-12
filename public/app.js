7283b4e3f160d639bef122549151b3bcdf7982cc
Mike <you@example.com>
2018-01-12 14:35:13 -0700

    took out the excess comments and corrected to two lines going through the screen and added a link to the creators name that takes him to an about me page

--- public/app.js
+++ public/app.js
@@ -5,8 +5,10 @@ var weatherForm = document.getElementById('weather-form');
 var promises = [];
 var happyBands = [
   'https://itunes.apple.com/search?term=lcdsoundsystem',
-  'https://itunes.apple.com/search?term=arcadefire', 'https://itunes.apple.com/search?term=foals',
-  'https://itunes.apple.com/search?term=menomena', 'https://itunes.apple.com/search?term=altj',
+  'https://itunes.apple.com/search?term=arcadefire',
+  'https://itunes.apple.com/search?term=foals',
+  'https://itunes.apple.com/search?term=menomena',
+  'https://itunes.apple.com/search?term=altj',
   'https://itunes.apple.com/search?term=animalcollective',
   'https://itunes.apple.com/search?term=gorillaz',
   'https://itunes.apple.com/search?term=futureislands',
@@ -15,7 +17,16 @@ var happyBands = [
 ]
 
 var sadBands = [
-  'https://itunes.apple.com/search?term=damienjurado', 'https://itunes.apple.com/search?term=sigurros', 'https://itunes.apple.com/search?term=avettbrothers', 'https://itunes.apple.com/search?term=brighteyes', 'https://itunes.apple.com/search?term=norahjones', 'https://itunes.apple.com/search?term=louisarmstrong', 'https://itunes.apple.com/search?term=ettajames', 'https://itunes.apple.com/search?term=anathallo', 'https://itunes.apple.com/search?term=beirut', 'https://itunes.apple.com/search?term=danieljohnston'
+  'https://itunes.apple.com/search?term=damienjurado',
+  'https://itunes.apple.com/search?term=sigurros',
+  'https://itunes.apple.com/search?term=avettbrothers',
+  'https://itunes.apple.com/search?term=brighteyes',
+  'https://itunes.apple.com/search?term=norahjones',
+  'https://itunes.apple.com/search?term=louisarmstrong',
+  'https://itunes.apple.com/search?term=ettajames',
+  'https://itunes.apple.com/search?term=anathallo',
+  'https://itunes.apple.com/search?term=beirut',
+  'https://itunes.apple.com/search?term=danieljohnston'
 ]
 
 var angryBands = [
@@ -68,130 +79,109 @@ weatherForm.addEventListener('submit', function(event) {
 
   var weatherEndpoint = 'https://galvanize-cors-proxy.herokuapp.com/http://api.wunderground.com/api/8f425deadec4abd7/conditions/q/';
   var weatherAPI = weatherEndpoint + stateSearchValue + "/" + citySearchValue + ".json";
-  fetch(weatherAPI)
-    .then((response) => {
-      return response.json()
-        .then ((weatherSearchData) => {
-          var weatherStatus = weatherSearchData.current_observation.weather;
-          var weatherRadar = document.getElementsByClassName('weather-radar')[0];
-          var cloudCoverage = weatherSearchData.current_observation.weather;
-          console.log(cloudCoverage);
-          var temperature = Math.round(weatherSearchData.current_observation.temp_f) + '˚F';
-          var weatherImage = weatherSearchData.current_observation.icon_url;
-          // ADD WEATHER TO HTML PAGE
-          // adding weather image
-          var eImg = document.createElement('img');
-          eImg.setAttribute('src', weatherImage);
-          weatherRadar.append(eImg)
-          eImg.className += "image-adj"
-          // adding temperature
-          var eH5 = document.createElement('h5');
-          eH5.innerHTML = temperature;
-          weatherRadar.append(eH5);
-          eH5.className += "font-adj"
-          //ADD MOOD EMOJI TO HTML PAGE
-          var moodImage = document.getElementsByClassName('mood-image')[0];
-          var happy = document.getElementById('moodChoice1')
-          var sad = document.getElementById('moodChoice2')
-          var angry = document.getElementById('moodChoice3')
-          var sexy = document.getElementById('moodChoice4')
-          var goofy = document.getElementById('moodChoice5')
-          if (happy.checked) {
-            var emoImg = document.createElement('img');
-            emoImg.setAttribute('src', 'emotions/happy.png')
-            moodImage.append(emoImg);
-            emoImg.className += "image-adj";
-            var emoChoice = 'happy';
-            getBands(happyBands);
-          } else if (sad.checked) {
-            var emoImg = document.createElement('img');
-            emoImg.setAttribute('src', 'emotions/sad.png')
-            moodImage.append(emoImg);
-            emoImg.className += "image-adj";
-            var emoChoice = 'sad';
-            getBands(sadBands);
-          } else if (angry.checked) {
-            var emoImg = document.createElement('img');
-            emoImg.setAttribute('src', 'emotions/angry.png')
-            moodImage.append(emoImg);
-            emoImg.className += "image-adj";
-            var emoChoice = 'angry';
-            getBands(angryBands);
-          } else if (sexy.checked) {
-            var emoImg = document.createElement('img');
-            emoImg.setAttribute('src', 'emotions/sexy.png')
-            moodImage.append(emoImg);
-            emoImg.className += "image-adj";
-            var emoChoice = 'sexy';
-            getBands(sexyBands);
-          } else if (goofy.checked) {
-            var emoImg = document.createElement('img');
-            emoImg.setAttribute('src', 'emotions/goofy.png')
-            moodImage.append(emoImg);
-            emoImg.className += "image-adj";
-            var emoChoice = 'goofy';
-            getBands(goofyBands)
-          } else {
-            var nothing = document.createElement('h5');
-            nothing.innerHTML = 'Pick an Emotion';
-            moodImage.append(nothing);
-            var emoChoice = 'nothing';
-          }
+  fetch(weatherAPI).then((response) => {
+    return response.json().then((weatherSearchData) => {
+      var weatherStatus = weatherSearchData.current_observation.weather;
+      var weatherRadar = document.getElementsByClassName('weather-radar')[0];
+      var cloudCoverage = weatherSearchData.current_observation.weather;
+      console.log(cloudCoverage);
+      var temperature = Math.round(weatherSearchData.current_observation.temp_f) + '˚F';
+      var weatherImage = weatherSearchData.current_observation.icon_url;
+      // ADD WEATHER TO HTML PAGE
+      // adding weather image
+      var eImg = document.createElement('img');
+      eImg.setAttribute('src', weatherImage);
+      weatherRadar.append(eImg)
+      eImg.className += "image-adj"
+      // adding temperature
+      var eH5 = document.createElement('h5');
+      eH5.innerHTML = temperature;
+      weatherRadar.append(eH5);
+      eH5.className += "font-adj"
+      //ADD MOOD EMOJI TO HTML PAGE
+      var moodImage = document.getElementsByClassName('mood-image')[0];
+      var happy = document.getElementById('moodChoice1')
+      var sad = document.getElementById('moodChoice2')
+      var angry = document.getElementById('moodChoice3')
+      var sexy = document.getElementById('moodChoice4')
+      var goofy = document.getElementById('moodChoice5')
+      if (happy.checked) {
+        var emoImg = document.createElement('img');
+        emoImg.setAttribute('src', 'emotions/happy.png')
+        moodImage.append(emoImg);
+        emoImg.className += "image-adj";
+        var emoChoice = 'happy';
+        getBands(happyBands);
+      } else if (sad.checked) {
+        var emoImg = document.createElement('img');
+        emoImg.setAttribute('src', 'emotions/sad.png')
+        moodImage.append(emoImg);
+        emoImg.className += "image-adj";
+        var emoChoice = 'sad';
+        getBands(sadBands);
+      } else if (angry.checked) {
+        var emoImg = document.createElement('img');
+        emoImg.setAttribute('src', 'emotions/angry.png')
+        moodImage.append(emoImg);
+        emoImg.className += "image-adj";
+        var emoChoice = 'angry';
+        getBands(angryBands);
+      } else if (sexy.checked) {
+        var emoImg = document.createElement('img');
+        emoImg.setAttribute('src', 'emotions/sexy.png')
+        moodImage.append(emoImg);
+        emoImg.className += "image-adj";
+        var emoChoice = 'sexy';
+        getBands(sexyBands);
+      } else if (goofy.checked) {
+        var emoImg = document.createElement('img');
+        emoImg.setAttribute('src', 'emotions/goofy.png')
+        moodImage.append(emoImg);
+        emoImg.className += "image-adj";
+        var emoChoice = 'goofy';
+        getBands(goofyBands)
+      } else {
+        var nothing = document.createElement('h5');
+        nothing.innerHTML = 'Pick an Emotion';
+        moodImage.append(nothing);
+        var emoChoice = 'nothing';
+      }
 
-          function getBands(emotionBandArray) {
-            for (i = 0; i < emotionBandArray.length; i++) {
-              fetch(emotionBandArray[i])
-                .then((response) => {
-                  return response.json()
-                  }).then((data) => {
-                    promises.push(data);
-                    var playlist = document.getElementsByClassName('playlist')[0];
-                    for (var i = 0; i < promises.length; i++) {
-                      // SONG IMAGE
-                      var songImgURL = promises[i].results["0"].artworkUrl100;
-                      var songImg = document.createElement('img');
-                      songImg.setAttribute('src', songImgURL);
-                      songImg.className += "song-img-head-room";
-                      // ARTIST NAME and SONG NAME
-                      var artistSongTitle = document.createElement('p')
-                      var artistName = promises[i].results["0"].artistName;
-                      var songName = promises[i].results["0"].trackName;
-                      artistSongTitle.innerHTML = artistName + ' - ' + songName;
-                      // SONG PLAYER
-                      var songPreURL = promises[i].results["0"].previewUrl;
-                      var songPlayer = document.createElement('video');
-                      songPlayer.setAttribute('controls', 'controls');
-                      songPlayer.setAttribute('name', 'media');
-                      // SET SOURCE INSIDE VIDEO TAG
-                      var source = document.createElement('source');
-                      source.setAttribute('src', songPreURL);
-                      source.setAttribute('type', 'audio/x-m4a');
-                      songPlayer.append(source);
-                      // PAGE BREAK AFTER EACH PLAYLIST ITEM
-                      var pageBreak = document.createElement('br');
-                    }
-                    playlist.append(songImg, artistSongTitle, songPlayer, pageBreak);
-              })
+      function getBands(emotionBandArray) {
+        for (i = 0; i < emotionBandArray.length; i++) {
+          fetch(emotionBandArray[i]).then((response) => {
+            return response.json()
+          }).then((data) => {
+            promises.push(data);
+            var playlist = document.getElementsByClassName('playlist')[0];
+            for (var i = 0; i < promises.length; i++) {
+              // SONG IMAGE
+              var songImgURL = promises[i].results["0"].artworkUrl100;
+              var songImg = document.createElement('img');
+              songImg.setAttribute('src', songImgURL);
+              songImg.className += "song-img-head-room";
+              // ARTIST NAME and SONG NAME
+              var artistSongTitle = document.createElement('p')
+              var artistName = promises[i].results["0"].artistName;
+              var songName = promises[i].results["0"].trackName;
+              artistSongTitle.innerHTML = artistName + ' - ' + songName;
+              // SONG PLAYER
+              var songPreURL = promises[i].results["0"].previewUrl;
+              var songPlayer = document.createElement('video');
+              songPlayer.setAttribute('controls', 'controls');
+              songPlayer.setAttribute('name', 'media');
+              // SET SOURCE INSIDE VIDEO TAG
+              var source = document.createElement('source');
+              source.setAttribute('src', songPreURL);
+              source.setAttribute('type', 'audio/x-m4a');
+              songPlayer.append(source);
+              // PAGE BREAK AFTER EACH PLAYLIST ITEM
+              var pageBreak = document.createElement('br');
             }
+            playlist.append(songImg, artistSongTitle, songPlayer, pageBreak);
+          })
         }
-          // CREATE EMPTY ARRAY FOR FETCHING PROMISES (PUSH)
-          // ARRAY OF BAND NAMES
-          // LOOP THROU ARRAY
-            //  GEN. URL (ENDPOINT)
-            //  FETCH URL
-            //  PUSH RESULT=(promise) OF FETCH TO EMPTY ARRAY
-          // PROMISE.ALL ON EMPTY ARRAY
-
-          //Promise.all(happyPromises).then((values) => {
-
-            // var table = document.getElementsByClassName('playlist-table')[0];
-            // for (var i = 0; i < happyPromises.length; i++) {
-            //   //adding song IMAGE
-            //   console.log(happyPromises[i]);
-            //   // var songImgURL = values["0"].results[i].artworkUrl30;
-            //
-            // }
-          //})
-        })
+      }
     })
+  })
 })
